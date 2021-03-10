8f6a00406d00bea5d30e53c701cb9a33435e5b2d
Elizabeth <gresdiversaidade@gmail.com>
2021-03-10 10:42:17 -0300

    Initial commit

---
+++ js/init.js
@@ -0,0 +1,11 @@
+// Such info must be obtained from Firebase Project, on firebase console
+var firebaseConfig = {
+  apiKey: "AIzaSyDfG0vMBe70apZxQzHZvdHfgGcxp5C2Ifo",
+  authDomain: "todo-list-3b386.firebaseapp.com",
+  projectId: "todo-list-3b386",
+  storageBucket: "todo-list-3b386.appspot.com",
+  messagingSenderId: "1022957400958",
+  appId: "1:1022957400958:web:587854662640fa07d8f92f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
